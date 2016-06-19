8e426458087c5e3226cec8fe03f75d38c72a59fb
xtne6f <xt4ubq@gmail.com>
2016-06-19 20:47:12 +0900

    Do not echo back user-controllable input
    Such verbose responses can cause XSS in some cases.

--- src/civetweb.c
+++ src/civetweb.c
@@ -14999,8 +14999,8 @@ handle_request(struct mg_connection *conn)
 			 * PUT/DELETE methods are not valid. */
 			mg_send_http_error(conn,
 			                   405,
-			                   "%s method not allowed",
-			                   conn->request_info.request_method);
+			                   "%s",
+			                   "Method not allowed");
 			DEBUG_TRACE("%s", "all file based put/delete requests rejected");
 			return;
 		}
@@ -15223,8 +15223,8 @@ handle_request(struct mg_connection *conn)
 		 * only for scripts (Lua, CGI) and callbacks. */
 		mg_send_http_error(conn,
 		                   405,
-		                   "%s method not allowed",
-		                   conn->request_info.request_method);
+		                   "%s",
+		                   "Method not allowed");
 		DEBUG_TRACE("method %s on %s is not supported",
 		            ri->request_method,
 		            path);
@@ -15295,8 +15295,8 @@ handle_request(struct mg_connection *conn)
 	    && (0 != strcmp(ri->request_method, "HEAD"))) {
 		mg_send_http_error(conn,
 		                   405,
-		                   "%s method not allowed",
-		                   conn->request_info.request_method);
+		                   "%s",
+		                   "Method not allowed");
 		DEBUG_TRACE("handling %s request to %s done", ri->request_method, path);
 		return;
 	}
@@ -19410,8 +19410,7 @@ process_new_connection(struct mg_connection *conn)
 			            NULL, /* No truncation check for ebuf */
 			            ebuf,
 			            sizeof(ebuf),
-			            "Bad HTTP version: [%s]",
-			            ri->http_version);
+			            "Bad HTTP version");
 			mg_send_http_error(conn, 505, "%s", ebuf);
 		}
 
