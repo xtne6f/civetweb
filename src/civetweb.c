fe43724d5c92cc59d17bb1accfcd65157638e59d
xtne6f <xt4ubq@gmail.com>
2017-10-19 05:11:13 +0900

    Fix resource leaks about SSL

--- src/civetweb.c
+++ src/civetweb.c
@@ -15990,6 +15990,7 @@ initialize_openssl(char *ebuf, size_t ebuf_len)
 			            __func__,
 			            ssl_error());
 			DEBUG_TRACE("%s", ebuf);
+			mg_atomic_dec(&cryptolib_users);
 			return 0;
 		}
 
@@ -16006,6 +16007,7 @@ initialize_openssl(char *ebuf, size_t ebuf_len)
 				            num_locks);
 				DEBUG_TRACE("%s", ebuf);
 				mg_free(ssl_mutexes);
+				mg_atomic_dec(&cryptolib_users);
 				return 0;
 			}
 		}
@@ -16024,6 +16026,7 @@ initialize_openssl(char *ebuf, size_t ebuf_len)
 			mg_free(ssl_mutexes);
 #endif
 			DEBUG_TRACE("%s", ebuf);
+			mg_atomic_dec(&cryptolib_users);
 			return 0;
 		}
 	}
@@ -16657,9 +16660,12 @@ init_ssl_ctx(struct mg_context *phys_ctx, struct mg_domain_context *dom_ctx)
 	} else if (callback_ret > 0) {
 		/* Callback > 0: Consider init done. */
 		dom_ctx->ssl_ctx = (SSL_CTX *)ssl_ctx;
-		if (!initialize_openssl(ebuf, sizeof(ebuf))) {
-			mg_cry_ctx_internal(phys_ctx, "%s", ebuf);
-			return 0;
+		if (!mg_openssl_initialized) {
+			if (!initialize_openssl(ebuf, sizeof(ebuf))) {
+				mg_cry_ctx_internal(phys_ctx, "%s", ebuf);
+				return 0;
+			}
+			mg_openssl_initialized = 1;
 		}
 		return 1;
 	}
@@ -21404,6 +21410,16 @@ mg_exit_library(void)
 			uninitialize_openssl();
 			mg_openssl_initialized = 0;
 		}
+#if !defined(NO_SSL_DL)
+		if (ssllib_dll_handle) {
+			(void)dlclose(ssllib_dll_handle);
+			ssllib_dll_handle = NULL;
+		}
+		if (cryptolib_dll_handle) {
+			(void)dlclose(cryptolib_dll_handle);
+			cryptolib_dll_handle = NULL;
+		}
+#endif
 #endif
 
 #if defined(_WIN32)
