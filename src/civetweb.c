GerHobbelt/civet-webserver

Security improvement of global_auth_file authorization
The authorization should be denied when fopen() fails.

src/civetweb.c
@@ -4658,6 +4658,8 @@ static int check_authorization(struct mg_connection *conn, const char *path)
 	if (is_file_opened(&file)) {
 		authorized = authorize(conn, &file);
 		mg_fclose(&file);
+	} else if (conn->ctx->config[GLOBAL_PASSWORDS_FILE] != NULL) {
+		authorized = 0;
 	}
 
 	return authorized;

xtne6f/civetweb

src/civetweb.c
@@ -8958,6 +8958,8 @@ check_authorization(struct mg_connection *conn, const char *path)
 	if (is_file_opened(&file.access)) {
 		authorized = authorize(conn, &file, NULL);
 		(void)mg_fclose(&file.access); /* ignore error on read only file */
+	} else if (conn->dom_ctx->config[GLOBAL_PASSWORDS_FILE] != NULL) {
+		authorized = 0;
 	}
 
 	return authorized;